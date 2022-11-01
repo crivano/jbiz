40335a87445eaa404830f631d5f7880a53c1fe8e
Renato Crivano <renato.crivano@gmail.com>
2022-11-01 11:35:25 -0300

    Adding a group and name method

--- src/main/java/com/crivano/jbiz/IGroupBy.java
+++ src/main/java/com/crivano/jbiz/IGroupBy.java
@@ -4,4 +4,8 @@ public interface IGroupBy extends IItem {
 	public static final String GROUP_SEPARATOR = ": ";
 
 	public String getGroup();
+
+	default public String getGroupAndName() {
+		return getGroup() + GROUP_SEPARATOR + getDescr();
+	};
 }
